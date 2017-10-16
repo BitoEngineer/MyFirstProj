a9907103fd859ffa843e47462ea5b55fc37a3972
DESKTOP-73V1DGD\BitoEngineer <bito_asd@hotmail.com>
2017-10-16 10:47:47 +0100

    Login almost done

--- TouchIt Faster/Assets/Scripts/Preloader/PreloaderManager.cs
+++ TouchIt Faster/Assets/Scripts/Preloader/PreloaderManager.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using System;
 
 public class PreloaderManager : MonoBehaviour {
 
@@ -9,6 +13,32 @@ public class PreloaderManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+  
+
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+       // enables saving game progress.
+       .EnableSavedGames()
+       // registers a callback to handle game invitations received while the game is not running.
+       .WithInvitationDelegate(null)
+       // registers a callback for turn based match notifications received while the
+       // game is not running.
+       .WithMatchDelegate(null)
+       // requests the email address of the player be available.
+       // Will bring up a prompt for consent.
+       .RequestEmail()
+       // requests a server auth code be generated so it can be passed to an
+       //  associated back end server application and exchanged for an OAuth token.
+       .RequestServerAuthCode(false)
+       // requests an ID token be generated.  This OAuth token can be used to
+       //  identify the player to other services such as Firebase.
+       .RequestIdToken()
+       .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
     }
 	
 	// Update is called once per frame
@@ -19,4 +49,7 @@ public class PreloaderManager : MonoBehaviour {
             SceneManager.LoadScene("Main Menu");
         }
 	}
+
+    public delegate void InvitationDelegate();
+    public delegate void MatchDelegate();
 }
