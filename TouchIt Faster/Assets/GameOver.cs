c9db669cb3719ea0f04d4268cd8cc3bebeafe3ca
marco_agostinho <marco.agostinho@frotcom.com>
2020-05-25 18:18:37 +0100

    Improvements in Single Player -> Marco

---
+++ TouchIt Faster/Assets/GameOver.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    private TouchManager touchManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        touchManager = GameObject.Find("Touch").GetComponent<TouchManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Continue(GameObject countDown)
+    {    
+        if (!touchManager.AdSeen)
+        {
+            //AdManager.Instance.showInterstitialAd();
+            touchManager.AdSeen = true;
+            gameOverPanel.SetActive(false);
+
+            touchManager.CleanUpCircles();
+            touchManager.Life1.enabled = false;
+            touchManager.Life2.enabled = false;
+            touchManager.Life3.enabled = false;
+            touchManager.OnPause = false;
+            touchManager.Lifes = 3;
+            //timerCounter.GetComponent<TimerCounter>().ResumeTimer(); ;
+            countDown.GetComponent<CountDown>().StartCountDown(false);
+        }
+        else
+        {
+            //Generate text saying that AD already was used
+        }
+
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Single player");
+    }
+}
